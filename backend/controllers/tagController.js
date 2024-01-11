71c3f281d0ab44ff8d6a6a1e429190fe9b6bcb87
toanhq <haquangtoan99@gmail.com>
2024-01-12 00:10:27 +0700

    add tag mvc

--- backend/controllers/tagController.js
+++ backend/controllers/tagController.js
@@ -3,6 +3,7 @@ const {
   getSingleOrder,
   createOrder,
 } = require("../services/orderService");
+const tagService = require("../services/tagService");
 
 exports.create_order = async (req, res, next) => {
   const { userId, cart } = req.body;
@@ -41,3 +42,51 @@ exports.get_orders = async (req, res, next) => {
       res.status(statusCode).send({ message }) && next(err);
     });
 };
+
+exports.get_all_tags = (req, res) => {
+
+  tagService.getAllTags()
+      .then(result => {
+        const {data, total} = result;
+        res.json({ data, total });
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+};
+
+exports.get_tag_by_id = (req, res) => {
+  const { id } = req.params;
+
+  tagService.getTagById(id)
+      .then(result => {
+        res.json({ data: result });
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+};
+
+exports.create_tag = (req, res) => {
+  const tagData = req.body; // Giả sử thông tin coupon được gửi trong body của request
+
+  tagService.createTag(tagData)
+      .then(result => {
+        res.status(result.statusCode).json(result);
+      })
+      .catch(error => {
+        res.status(error.statusCode || 500).json(error);
+      });
+};
+
+exports.delete_tag = async (req, res) => {
+  const { id } = req.params;
+
+  tagService.deleteTag(id)
+      .then(result => {
+        res.json({ data: result });
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,11 +6,13 @@ const usersRoute = require("./users");
 const productsRoute = require("./products");
 const ordersRoute = require("./orders");
 const couponsRoute = require("./coupons");
+const tagsRoute = require("./tags");
 
 router.use("/api/v1/auth", authRoute);
 router.use("/api/v1/users", usersRoute);
 router.use("/api/v1/products", productsRoute);
 router.use("/api/v1/orders", ordersRoute);
 router.use("/api/v1/coupons", couponsRoute);
+router.use("/api/v1/tags", tagsRoute);
 
 module.exports = router;
